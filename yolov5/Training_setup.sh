9dc33df7cfe7bc554648314a9a3df8061eef1d6d
Praledon <44362204+Praledon@users.noreply.github.com>
2024-02-20 13:18:31 +0100

    Added requirements and classes for tunnel 7

--- yolov5/Training_setup.sh
+++ yolov5/Training_setup.sh
@@ -9,3 +9,5 @@
 uenv verbose cuda-11.4 cudnn-11.4-8.2.4
 uenv miniconda-py39
 conda create -n Training_env -c pytorch pytorch torchvision numpy -y
+conda activate Training_env
+pip install -r requirements.txt
